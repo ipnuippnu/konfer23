1082c1fccb22c7dfbdc9172729610cb39722d6e0
Isnu Nasrudin <isnunas@gmail.com>
2023-06-05 13:27:10 +0700

    Gas pusher

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -23,11 +23,11 @@ const Eko = new Echo({
     broadcaster: 'pusher',
     key: import.meta.env.VITE_PUSHER_APP_KEY,
     cluster: import.meta.env.VITE_PUSHER_APP_CLUSTER ?? 'mt1',
-    wsHost: import.meta.env.VITE_PUSHER_HOST ? import.meta.env.VITE_PUSHER_HOST : `ws-${import.meta.env.VITE_PUSHER_APP_CLUSTER}.pusher.com`,
-    wsPort: import.meta.env.VITE_PUSHER_PORT ?? 80,
-    wssPort: import.meta.env.VITE_PUSHER_PORT ?? 443,
+    wsHost: window.location.hostname,
+    wsPort: 80,
+    wssPort: 443,
     forceTLS: (import.meta.env.VITE_PUSHER_SCHEME ?? 'https') === 'https',
-    enabledTransports: ['ws'],
+    enabledTransports: ['ws', 'wss'],
 });
 
 Eko.connector.pusher.connection.bind('connecting', (payload) => {
--- resources/js/qr.js
+++ resources/js/qr.js
@@ -1,26 +1,26 @@
 import {Eko, axios, Pusher, Suwal} from './bootstrap.js';
 
-// Eko.channel(`qr_guest`)
-//     .listen('QrGuest', (e) => {
+Eko.channel(`qr_guest`)
+    .listen('QrGuest', (e) => {
 
-//         let timerInterval
-//         Suwal.fire({
-//         icon: 'success',
-//         title: 'Selamat Datang',
-//         html: e.name,
-//         timer: 2000,
-//         timerProgressBar: true,
-//         showConfirmButton:false,
-//         didOpen: () => {
-//             Swal.showLoading()
-//             const b = Swal.getHtmlContainer().querySelector('b')
-//             timerInterval = setInterval(() => {
-//             b.textContent = Swal.getTimerLeft()
-//             }, 300)
-//         },
-//         willClose: () => {
-//             clearInterval(timerInterval)
-//         }
-//         })
+        let timerInterval
+        Suwal.fire({
+        icon: 'success',
+        title: 'Selamat Datang',
+        html: e.name,
+        timer: 2000,
+        timerProgressBar: true,
+        showConfirmButton:false,
+        didOpen: () => {
+            Swal.showLoading()
+            const b = Swal.getHtmlContainer().querySelector('b')
+            timerInterval = setInterval(() => {
+            b.textContent = Swal.getTimerLeft()
+            }, 300)
+        },
+        willClose: () => {
+            clearInterval(timerInterval)
+        }
+        })
 
-//     });
+    });
